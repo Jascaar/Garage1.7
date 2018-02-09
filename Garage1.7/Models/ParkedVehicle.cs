1e3aeb0e204e19b379b97d0c82586bf2360f51ad
Rob Devv <rob.ternstrom@gmail.com>
2018-02-09 10:46:54 +0100

    Model created for ParkedVehicle with properties bound to a veghicle.

---
+++ Garage1.7/Models/ParkedVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage1._7.Models
+{
+    public class ParkedVehicle
+    {
+        public string typeOfVehicle { get; set; }
+        public string vehicleRegistrationNumber { get; set; }
+        public string color { get; set; }
+        public string vehicleModel { get; set; }
+        public int numberOfTiresForVehicle { get; set; }
+
+    }
+}
