19de1b6f2b9394ad5fd29e794901d4f14436085d
Rob Devv <rob.ternstrom@gmail.com>
2018-02-09 10:34:49 +0100

    Commit 2 personal branch

---
+++ Garage1.7/Models/Vehicles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage1._7.Models
+{
+    public class Vehicles
+    {
+        public int numberOfTires { get; set; }
+    }
+}
