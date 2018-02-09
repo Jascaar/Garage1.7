27c489ab78329967ffed8d32f738b750f2bdbe22
Jascaar <34939709+Jascaar@users.noreply.github.com>
2018-02-09 15:31:15 +0100

    Revert "Master branch."
    This reverts commit 14188df64822ac2ea881fc4c507ef4a1869dd2b4.

--- Garage1.7/App_Start/RouteConfig.cs
+++ Garage1.7/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Garage1._7
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ParkedVehicles", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
